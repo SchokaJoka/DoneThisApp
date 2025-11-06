b1afd0290858dc9cf5475900bdc7f59f0cddc3f1
Joel Kammermann <joel.kammermann@modual.ch>
2025-11-06 16:52:15 +0100

    hotfix: supabase error "parse"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,16 @@ export default defineNuxtConfig({
     // Private server-only token for Replicate
     replicateApiToken: process.env.REPLICATE_API_TOKEN
   },
+  hooks: {
+    'vite:extendConfig': extendViteConfig,
+  },
 })
+
+function extendViteConfig(config: import('vite').UserConfig) {
+   const plugin = config.plugins?.find(plugin => isPlugin(plugin, 'nuxt:environments'))
+   if (plugin) plugin.enforce = 'pre'
+}
+
+function isPlugin(plugin: unknown, name: string): plugin is import('vite').Plugin {
+   return !!(plugin && typeof plugin === 'object' && 'name' in plugin && plugin.name === name)
+}
